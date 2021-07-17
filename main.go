14b6885d21e8094628b57f231d169a4bf2b7ccfb
Kareem <kosa0001@student.monash.edu>
2021-07-17 15:38:19 +0300

    New commit

--- main.go
+++ main.go
@@ -2,20 +2,36 @@
 package main
 
 import (
-	"fmt" // just for printing something on the screen
+	// We will need these libraries:
+	//      // need to convert data into byte in order to be sent on the network, computer understands better the byte(8bits)language
+	//"crypto/sha256" //crypto library to hash the data
+	//"strconv"       // for conversion
+	"time" // the time for our timestamp
+
+	"github.com/davecgh/go-spew/spew"
+
+	//"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	//"encoding/hex"
+	//"encoding/json"
+	//"io"
+	"log"
+	//"net/http"
+	//"os"
 )
 
 func main() {
-	newblockchain := NewBlockchain() // Initialize the blockchain
-	// create 2 blocks and add 2 transactions
-	newblockchain.AddBlock("first transaction")  // first block containing one tx
-	newblockchain.AddBlock("Second transaction") // second block containing one tx
-	// Now print all the blocks and their contents
-	for i, block := range newblockchain.Blocks { // iterate on each block
-		fmt.Printf("Block ID : %d \n", i)                                        // print the block ID
-		fmt.Printf("Timestamp : %d \n", block.Timestamp+int64(i))                // print the timestamp of the block, to make them different, we just add a value i
-		fmt.Printf("Hash of the block : %x\n", block.MyBlockHash)                // print the hash of the block
-		fmt.Printf("Hash of the previous Block : %x\n", block.PreviousBlockHash) // print the hash of the previous block
-		fmt.Printf("All the transactions : %s\n", block.AllData)                 // print the transactions
-	} // our blockchain will be printed
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), "", "", "bruh"}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+	}()
+	log.Fatal(run())
+
 }
--- structures.go
+++ structures.go
@@ -2,13 +2,16 @@ package main //Import the main package
 // Create the Block data structure
 // A block contains this info:
 type Block struct {
-	Timestamp         int64  // the time when the block was created
-	PreviousBlockHash []byte // the hash of the previous block
-	MyBlockHash       []byte // the hash of the current block
-	AllData           []byte // the data or transactions (body info)
+	Index     int
+	Timestamp string // the time when the block was created
+	PrevHash  string // the hash of the previous block
+	Hash      string // the hash of the current block
+	AllData   string // the data or transactions (body info)
 }
 
-// Prepare the Blockchain data structure :
-type Blockchain struct {
-	Blocks []*Block // remember a blockchain is a series of blocks
-}
+// // Prepare the Blockchain data structure :
+// type Blockchain struct {
+// 	Blocks []*Block // remember a blockchain is a series of blocks
+// }
+
+var Blockchain []Block
